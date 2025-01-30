049258aae5f2cf61d93703a3430e1478a6ea8324
Bar Pinsker <barpinkser12@gmail.com>
2025-01-30 21:25:33 +0200

    Created a build container and copied necessary configuration files. Installed production dependencies and ensured that the build files are correctly transferred from the build container. Updated configuration files to be included in the runtime environment.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DesktopVehiclesComponent } from './desktop-vehicles/desktop-vehicles.component';
 
 const routes: Routes = [
-  { path: '/', redirectTo: '/desktop', pathMatch: 'full' },
+  { path: '', redirectTo: '/desktop', pathMatch: 'full' },
   { path: 'desktop', component: DesktopVehiclesComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Headers } from './desktop-vehicles//headerTable';
+import { RestApiService } from './service/rest-api.service';
+import { Pipe, PipeTransform } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +14,109 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  title = 'forntend';
+   constructor(private restApi: RestApiService,private toastr:ToastrService) {}
+   title = 'forntend';
+    carList: any[] = [];
+    headers: any = new Headers().headerTable;
+    headerInputsModel: any = new Headers().headerInputs;
+    newCar: any = {};
+ 
+  editRowCar: any = {};
+  indexFilterActive: number = 0;
+  statusCarFilter = [
+    { name: 'all', value: 'empty', active: true },
+    { name: 'active', value: 'active', active: false },
+    { name: 'inactive', value: 'inactive', active: false },
+  ];
+
+  ngOnInit(): void {
+    var jsonValidaitor:any={}
+    for(let header of this.headerInputsModel){
+      jsonValidaitor[header.nameEnglish]=['', Validators.required]
+      this.newCar[header.nameEnglish]=''
+      this.editRowCar[header.nameEnglish]=''
+    }
+    this.getAllCars('empty');
+    
+  }
+  isFormValid(): boolean {
+    return (this.newCar.licensePlate !== ''||this.editRowCar.licensePlate !== '') && (this.newCar.manufacturer !== ''||this.editRowCar.manufacturer !== '') && (this.newCar.model !== ''||this.editRowCar.model !== '') && (this.newCar.status !== ''||this.editRowCar.status !== '');
+  }
+  getAllCars(status: string) {
+    this.restApi.getAllCars(`${status}`).subscribe(
+      (response) => {
+        console.log(response)
+        this.carList = [...response];
+       
+      },
+      (error) => {
+        console.log('not found cars in a list');
+      }
+    );
+  }
+  selectFilter(indexRow: number) {
+    this.statusCarFilter[indexRow].active = true;
+    this.getAllCars(this.statusCarFilter[indexRow].value);
+    if (this.indexFilterActive != indexRow) {
+      this.statusCarFilter[this.indexFilterActive].active = false;
+      this.indexFilterActive = indexRow;
+    }
+  }
+
+  getNewId() {
+    this.restApi.getNewId().subscribe((data) => {
+      this.newCar['id'] = data;
+    });
+  }
+  getSpecificCar(id: string) {
+    this.restApi.getSpecificCar(`${id}`).subscribe((response) => {
+      console.log(response)
+      this.editRowCar = response.data;
+    });
+  }
+  createNewCar() {
+    this.newCar.createdAt = formatDate(new Date(), 'dd-MM-yyyy', 'en');
+    this.newCar.updatedAt = formatDate(new Date(), 'dd-MM-yyyy', 'en');
+    this.restApi.createNewCar({data:this.newCar}).subscribe(
+      (response:any) => {
+        this.carList.push(response.data);
+        this.newCar = {};
+        this.toastr.success(response.message,"Create Car")
+      },
+      (error) => {
+        console.log(error)
+        this.toastr.error(error.error.message,"Create Car")
+      }
+    );
+  }
+  updateCar(id: number) {
+    this.editRowCar.updatedAt = formatDate(new Date(), 'dd-MM-yyyy', 'en');
+    this.restApi.updateCar(`${id}`, this.editRowCar).subscribe(
+      (response) => {
+        this.carList[this.carList.findIndex((car) => car.id === id)] =
+          this.editRowCar;
+      
+        this.toastr.success(response.message,"Update Car")
+      },
+      (error) => {
+        this.toastr.error(error.error.message,"Update Car")
+       
+      }
+    );
+  }
+  removeCar(id: string) {
+    this.restApi.deleteCar(`${id}`).subscribe(
+      (response) => {
+        const indexCarRemove = this.carList.findIndex((car) => car.id === id);
+        if (indexCarRemove !== -1) {
+          this.carList.splice(indexCarRemove, 1);
+        }
+        this.toastr.success(response.message,"Remove car")
+      },
+      
+      (error) => {
+        this.toastr.success(error.error.message,"Remove car")
+      }
+    );
+  }
 }
